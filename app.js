695156a5afbfe55ac179df14483119379a25f9c3
LucasAAlv <lucasev76@gmail.com>
2024-09-13 20:34:32 -0300

    update

--- app.js
+++ app.js
@@ -5,3 +5,4 @@ let numeroAleatorio = Math.random().toFixed(1) * 10;
 numero.textContent = 'Seu numero aleatório é: ' + numeroAleatorio;
 
 console.log("Hello, World!");
+console.log("Hello, World!!");
