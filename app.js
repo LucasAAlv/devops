9bc5b29e7f45289637cbeca0237eeaccbde2b34a
LucasAAlv <lucasev76@gmail.com>
2024-09-13 21:27:08 -0300

    using JQuery to manipulate the button behavior

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 let numero = $('p');
 
-let numeroAleatorio = Math.random().toFixed(1) * 10;
+let numeroAleatorio = (Math.random()* 10).toFixed(1) ;
 
 numero.html('Seu numero aleatório é: ' + numeroAleatorio)
 
+$(document).ready(function(){$('#clickMeButton').click(moveClickMeButton)})
+
+function moveClickMeButton(event){
+    event.preventDefault();
+    
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    var buttonWidth = $(event.target).outerWidth();
+    var buttonHeight = $(event.target).outerHeight();
+
+    var randomX = Math.random() * (windowWidth - buttonWidth);
+    var randomY = Math.random() * (windowHeight - buttonHeight);
+
+    $(event.target).css({
+    'left': randomX + 'px',
+    'top': randomY + 'px',
+    'position': 'absolute'
+    });
+}
 
