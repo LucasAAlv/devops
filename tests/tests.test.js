27628b2b16c79ec77cba9222559fb67e6147961f
LucasAAlv <lucasev76@gmail.com>
2024-09-29 14:27:41 -0300

    Add discord webhook

--- tests/tests.test.js
+++ tests/tests.test.js
@@ -1,6 +1,5 @@
 const { JSDOM } = require('jsdom');
 
-// Configuração do DOM simulado com jsdom
 const dom = new JSDOM(`
   <!DOCTYPE html>
   <html lang="en">
@@ -21,7 +20,6 @@ const dom = new JSDOM(`
 global.window = dom.window;
 global.document = dom.window.document;
 
-// Função moveClickMeButton simulada para os testes
 function moveClickMeButton(event) {
     event.preventDefault();
 
@@ -42,30 +40,25 @@ function moveClickMeButton(event) {
 describe('App Functionality Tests', () => {
 
     it('should generate a random number and display in paragraph', () => {
-        // Simula a geração de um número aleatório
         let numeroAleatorio = (Math.random() * 10).toFixed(1);
         const p = document.querySelector('p');
         p.textContent = 'Seu numero aleatório é: ' + numeroAleatorio;
 
-        // Testa se o parágrafo contém o número gerado
         expect(p.textContent).toContain('Seu numero aleatório é: ');
         expect(parseFloat(numeroAleatorio)).toBeGreaterThanOrEqual(0);
         expect(parseFloat(numeroAleatorio)).toBeLessThanOrEqual(10);
     });
 
     it('should move the button to a random position when clicked', () => {
-        // Simula o clique no botão
         const button = document.querySelector('#clickMeButton');
         const event = new dom.window.MouseEvent('click', { bubbles: true, cancelable: true });
 
-        // Captura as posições antes do clique
+
         const initialLeft = button.style.left;
         const initialTop = button.style.top;
 
-        // Executa a função de movimentação
         moveClickMeButton({ target: button, preventDefault: () => {} });
 
-        // Verifica se as novas posições são diferentes das anteriores
         const newLeft = button.style.left;
         const newTop = button.style.top;
 
@@ -74,7 +67,6 @@ describe('App Functionality Tests', () => {
     });
 
     it('should trigger the moveClickMeButton function when button is clicked', () => {
-        // Simula o clique no botão e verifica se a função é chamada
         const button = document.querySelector('#clickMeButton');
         const spyEvent = jest.fn();
         button.addEventListener('click', spyEvent);
@@ -89,10 +81,8 @@ describe('App Functionality Tests', () => {
         const button = document.querySelector('#clickMeButton');
         const event = new dom.window.MouseEvent('click', { bubbles: true, cancelable: true });
 
-        // Simula o clique e move o botão
         moveClickMeButton({ target: button, preventDefault: () => {} });
 
-        // Verifica se o botão está dentro dos limites da janela
         const leftPosition = parseFloat(button.style.left);
         const topPosition = parseFloat(button.style.top);
         const windowWidth = window.innerWidth;
